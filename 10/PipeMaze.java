9aad8561014b90a962d1e689c48b08245b03c337
Andrey Glotov <andrei.glotoff@gmail.com>
2023-12-11 10:16:44 +0300

    refactor solution to day 10

--- 10/PipeMaze.java
+++ 10/PipeMaze.java
@@ -2,26 +2,50 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Set;
 
 public class PipeMaze {
-  private static boolean hasConnectionToNorth(char c) {
-    return c == 'S' || c == '|' || c == 'L' || c == 'J';
-  }
+  private static class Coordinates {
+    private int i;
+    private int j;
+    private char symbol;
 
-  private static boolean hasConnectionToSouth(char c) {
-    return c == 'S' || c == '|' || c == '7' || c == 'F';
-  }
+    public Coordinates(int i, int j, char symbol) {
+      this.i = i;
+      this.j = j;
+      this.symbol = symbol;
+    }
 
-  private static boolean hasConnectionToEast(char c) {
-    return c == 'S' || c == '-' || c == 'L' || c == 'F';
-  }
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == this) return true;
+      if (obj == null) return false;
+      if (obj.getClass() != this.getClass()) return false;
 
-  private static boolean hasConnectionToWest(char c) {
-    return c == 'S' || c == '-' || c == '7' || c == 'J';
-  }
+      Coordinates c = (Coordinates) obj;
+      return (c.i == this.i) && (c.j == this.j) && (c.symbol == this.symbol);
+    }
 
-  private static String toStringKey(int i, int j) {
-    return i + "," + j;
+    @Override
+    public int hashCode() {
+      return 31 * (31 * i + j) + symbol;
+    }
+
+    public boolean hasConnectionToNorth() {
+      return symbol == 'S' || symbol == '|' || symbol == 'L' || symbol == 'J';
+    }
+
+    public boolean hasConnectionToSouth() {
+      return symbol == 'S' || symbol == '|' || symbol == '7' || symbol == 'F';
+    }
+
+    public boolean hasConnectionToEast() {
+      return symbol == 'S' || symbol == '-' || symbol == 'L' || symbol == 'F';
+    }
+
+    public boolean hasConnectionToWest() {
+      return symbol == 'S' || symbol == '-' || symbol == '7' || symbol == 'J';
+    }
   }
 
   public static void main(String[] args) {
@@ -36,55 +60,56 @@ public class PipeMaze {
     int mazeWidth = maze.get(0).length();
     int mazeHeight = maze.size();
 
-    HashMap<String, ArrayList<String>> adjacent = new HashMap<>();
-    String start = "0,0";
+    HashMap<Coordinates, ArrayList<Coordinates>> adjacent = new HashMap<>();
+    Coordinates start = null;
 
     for (int i = 0; i < mazeHeight; i++) {
       for (int j = 0; j < mazeWidth; j++) {
-        adjacent.put(i + "," + j, new ArrayList<>());
+        Coordinates tile = new Coordinates(i, j, maze.get(i).charAt(j));
+        adjacent.put(tile, new ArrayList<>());
 
-        if (maze.get(i).charAt(j) == 'S') {
-          start = toStringKey(i, j);
+        if (tile.symbol == 'S') {
+          start = tile;
         }
       }
     }
 
     for (int i = 0; i < mazeHeight; i++) {
       for (int j = 0; j < mazeWidth; j++) {
-        char current = maze.get(i).charAt(j);
+        Coordinates current = new Coordinates(i, j, maze.get(i).charAt(j));
 
         if (i > 0) {
-          char top = maze.get(i - 1).charAt(j);
-          if (hasConnectionToNorth(current) && hasConnectionToSouth(top)) {
-            adjacent.get(toStringKey(i, j)).add(toStringKey(i - 1, j));
-            adjacent.get(toStringKey(i - 1, j)).add(toStringKey(i, j));
+          Coordinates top = new Coordinates(i-1, j, maze.get(i-1).charAt(j));
+          if (current.hasConnectionToNorth() && top.hasConnectionToSouth()) {
+            adjacent.get(current).add(top);
+            adjacent.get(top).add(current);
           }
         }
 
         if (j > 0) {
-          char left = maze.get(i).charAt(j - 1);
-          if (hasConnectionToWest(current) && hasConnectionToEast(left)) {
-            adjacent.get(toStringKey(i, j)).add(toStringKey(i, j - 1));
-            adjacent.get(toStringKey(i, j - 1)).add(toStringKey(i, j));
+          Coordinates left = new Coordinates(i, j-1, maze.get(i).charAt(j-1));
+          if (current.hasConnectionToWest() && left.hasConnectionToEast()) {
+            adjacent.get(current).add(left);
+            adjacent.get(left).add(current);
           }
         }
       }
     }
 
-    HashMap<String, Integer> distance = new HashMap<>();
-    distance.put(start, 0);
+    HashMap<Coordinates, Integer> distanceTo = new HashMap<>();
+    distanceTo.put(start, 0);
     int maxDist = 0;
 
-    LinkedList<String> queue = new LinkedList<>();
+    LinkedList<Coordinates> queue = new LinkedList<>();
     queue.add(start);
 
     while (!queue.isEmpty()) {
-      String current = queue.removeFirst();
-      int distToAdjacent = distance.get(current) + 1;
+      Coordinates current = queue.removeFirst();
+      int distToAdjacent = distanceTo.get(current) + 1;
 
-      for (String tile: adjacent.get(current)) {
-        if (!distance.containsKey(tile)) {
-          distance.put(tile, distToAdjacent);
+      for (Coordinates tile: adjacent.get(current)) {
+        if (!distanceTo.containsKey(tile)) {
+          distanceTo.put(tile, distToAdjacent);
           queue.addLast(tile);
 
           maxDist = Math.max(maxDist, distToAdjacent);
@@ -94,39 +119,40 @@ public class PipeMaze {
 
     System.out.println(maxDist);
 
+    Set<Coordinates> loop = distanceTo.keySet();
     int insideCount = 0;
 
     for (int i = 0; i < mazeHeight; i++) {
       boolean inside = false;
-      int wall = 0;
 
       for (int j = 0; j < mazeWidth; j++) {
-        String key = toStringKey(i, j);
-        char tile = maze.get(i).charAt(j);
+        Coordinates current = new Coordinates(i, j, maze.get(i).charAt(j));
 
-        if (!distance.containsKey(key)) {
-          wall = 0;
-
+        if (!loop.contains(current)) {
           if (inside) {
             insideCount++;
           }
-        } else {
-          if (tile == '|') {
+        } else if (current.symbol == '|') {
+          inside = !inside;
+        } else if (current.symbol == 'F') {
+          char wallEnd;
+
+          do {
+            wallEnd = maze.get(i).charAt(++j);
+          } while (wallEnd == '-');
+
+          if (wallEnd == 'J') {
             inside = !inside;
-          } else {
-            if (wall == 0) {
-              wall = tile;
-            } else if (tile == 'J') {
-              if (wall == 'F') {
-                inside = !inside;
-              }
-              wall = 0;
-            } else if (tile == '7') {
-              if (wall == 'L') {
-                inside = !inside;
-              }
-              wall = 0;
-            }
+          }
+        } else if (current.symbol == 'L') {
+          char wallEnd;
+
+          do {
+            wallEnd = maze.get(i).charAt(++j);
+          } while (wallEnd == '-');
+
+          if (wallEnd == '7') {
+            inside = !inside;
           }
         }
       }
